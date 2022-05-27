williamtu/ovs

commit edb2335861d629f791467f77729e743378dac15f

datapath-windows: Add IPv6 Geneve tunnel support in Windows
In the first step OVS Windows will support IPv6 tunnel(Geneve IPv6 tunnel).

Implementation on Windows
-------------------------
1. For the IPv6 tunnel support,  OvsIPTunnelKey will replace original
OvsIPv4TunnelKey in the related flow context handing.
2. The related src and dst address will be  changed to SOCKADDR_INET type from UINT32.
3. For the IPv6 tunnel,  one node running OVS-Windows could encapsulate IPv4/IPv6
Packets via IPV6 Geneve Tunnel, and the node could also encapsulate IPv4/IPv6 packet
Via IPv4 Geneve tunnel.
4. Related IPHelper data structure will be adapted to support IPv6 Tunnel. In the IPHelper
part the related Windows API(such as GetUnicastIpAddressTable/GetBestRoute2/GetIpNetEntry2/
ResolveIpNetEntry2) and Windows data structure(MIB_IPFORWARD_ROW2/MIB_IPNET_ROW2/IP_ADDRESS_PREFIX)
Have already supported both IPv4 and IPV6. Now OVS Windows has been adjusted some functions
And data structured to support IPV6 tunnel also.
5. OVS_TUNNEL_KEY_ATTR_IPV6_SRC and OVS_TUNNEL_KEY_ATTR_IPV6_DST filed will be supported in
OVS-Windows kernel for IPV6 tunnel.

Testing done.
-------------------------
    Related topo, 1 Windows VM(Win2019) and 2 Ubuntu 16.04 server. Both VMs
Are running on one ESX host.
1. Setup one IPV6 Geneve Tunnel between 1 Windows VM and 1 Ubuntu server.
    Windows VM,  vif0( 6000::2/40.1.1.10) vif1(5000::2)—— Ubuntu VM Eth2(5000::9), name space ns1
    with interface ns1_link_peer(6000::9/40.1.1.2)
    Related tunnnel,
ovs-vsctl.exe add-port br-int bms-tun0 -- set interface bms-tun0 type=Geneve options:csum=true
    options:key=flow options:local_ip="5000::2" options:remote_ip=flow

     In this topo, traffic from Vif0(Win) to ns1_link_peer(Ubuntu) will be gone through the Geneve tunnel
(5000::2—>5000::9) for both IPv4 traffic(40.1.1.10-->40.1.1.2) and IPv6 traffic(6000::2—>6000::9)

2. Setup one IPV4 Geneve Tunnel between Windows VM and 1 Ubuntu server.

datapath-windows/ovsext/Gre.c
@@ -37,7 +37,7 @@
 
 static NDIS_STATUS
 OvsDoEncapGre(POVS_VPORT_ENTRY vport, PNET_BUFFER_LIST curNbl,
-              const OvsIPv4TunnelKey *tunKey,
+              const OvsIPTunnelKey *tunKey,
               const POVS_FWD_INFO fwdInfo,
               POVS_PACKET_HDR_INFO layers,
               POVS_SWITCH_CONTEXT switchContext,
@@ -93,7 +93,7 @@ OvsCleanupGreTunnel(POVS_VPORT_ENTRY vport)
 NDIS_STATUS
 OvsEncapGre(POVS_VPORT_ENTRY vport,
             PNET_BUFFER_LIST curNbl,
-            OvsIPv4TunnelKey *tunKey,
+            OvsIPTunnelKey *tunKey,
             POVS_SWITCH_CONTEXT switchContext,
             POVS_PACKET_HDR_INFO layers,
             PNET_BUFFER_LIST *newNbl,
@@ -102,7 +102,12 @@ OvsEncapGre(POVS_VPORT_ENTRY vport,
     OVS_FWD_INFO fwdInfo;
     NDIS_STATUS status;
 
-    status = OvsLookupIPFwdInfo(tunKey->src, tunKey->dst, &fwdInfo);
+    if (tunKey->dst.si_family != AF_INET) {
+        /*V6 tunnel support will be supported later*/
+        return NDIS_STATUS_FAILURE;
+    }
+
+    status = OvsLookupIPhFwdInfo(tunKey->src, tunKey->dst, &fwdInfo);
     if (status != STATUS_SUCCESS) {
         OvsFwdIPHelperRequest(NULL, 0, tunKey, NULL, NULL, NULL);
         return NDIS_STATUS_FAILURE;
@@ -124,7 +129,7 @@ OvsEncapGre(POVS_VPORT_ENTRY vport,
 NDIS_STATUS
 OvsDoEncapGre(POVS_VPORT_ENTRY vport,
               PNET_BUFFER_LIST curNbl,
-              const OvsIPv4TunnelKey *tunKey,
+              const OvsIPTunnelKey *tunKey,
               const POVS_FWD_INFO fwdInfo,
               POVS_PACKET_HDR_INFO layers,
               POVS_SWITCH_CONTEXT switchContext,
@@ -239,10 +244,11 @@ OvsDoEncapGre(POVS_VPORT_ENTRY vport,
                           IP_DF_NBO : 0;
         ipHdr->ttl = tunKey->ttl ? tunKey->ttl : 64;
         ipHdr->protocol = IPPROTO_GRE;
-        ASSERT(tunKey->dst == fwdInfo->dstIpAddr);
-        ASSERT(tunKey->src == fwdInfo->srcIpAddr || tunKey->src == 0);
-        ipHdr->saddr = fwdInfo->srcIpAddr;
-        ipHdr->daddr = fwdInfo->dstIpAddr;
+        ASSERT(OvsIphAddrEquals(&tunKey->dst, &fwdInfo->dstIphAddr));
+        ASSERT(OvsIphAddrEquals(&tunKey->src, &fwdInfo->srcIphAddr) ||
+               OvsIphIsZero(&tunKey->src));
+        ipHdr->saddr = fwdInfo->srcIphAddr.Ipv4.sin_addr.s_addr;
+        ipHdr->daddr = fwdInfo->dstIphAddr.Ipv4.sin_addr.s_addr;
 
         ipHdr->check = 0;
         ipHdr->check = IPChecksum((UINT8 *)ipHdr, sizeof *ipHdr, 0);
@@ -306,7 +312,7 @@ ret_error:
 NDIS_STATUS
 OvsDecapGre(POVS_SWITCH_CONTEXT switchContext,
             PNET_BUFFER_LIST curNbl,
-            OvsIPv4TunnelKey *tunKey,
+            OvsIPTunnelKey *tunKey,
             PNET_BUFFER_LIST *newNbl)
 {
     PNET_BUFFER curNb;
@@ -325,6 +331,12 @@ OvsDecapGre(POVS_SWITCH_CONTEXT switchContext,
     ASSERT(*newNbl == NULL);
 
     *newNbl = NULL;
+
+    if (tunKey->dst.si_family != AF_INET) {
+        /*V6 tunnel support will be supported later*/
+        return NDIS_STATUS_FAILURE;
+    }
+
     status = OvsExtractLayers(curNbl, &layers);
     if (status != NDIS_STATUS_SUCCESS) {
         return status;
@@ -363,8 +375,11 @@ OvsDecapGre(POVS_SWITCH_CONTEXT switchContext,
     headRoom += layers.l3Offset;
 
     ipHdr = (IPHdr *)(bufferStart + layers.l3Offset);
-    tunKey->src = ipHdr->saddr;
-    tunKey->dst = ipHdr->daddr;
+    tunKey->src.Ipv4.sin_addr.s_addr = ipHdr->saddr;
+    tunKey->src.Ipv4.sin_family = AF_INET;
+    tunKey->dst.Ipv4.sin_addr.s_addr = ipHdr->daddr;
+    tunKey->dst.Ipv4.sin_family = AF_INET;
+
     tunKey->tos = ipHdr->tos;
     tunKey->ttl = ipHdr->ttl;
     tunKey->pad = 0;

datapath-windows/ovsext/Actions.c
@@ -186,19 +186,29 @@ OvsDetectTunnelRxPkt(OvsForwardingContext *ovsFwdCtx,
      * packets only if they are at least VXLAN header size.
      */
 
-    /*
-     * For some of the tunnel types such as GRE, the dstPort is not applicable
-     * since GRE does not have a L4 port. We use '0' for convenience.
-     */
-    if (!flowKey->ipKey.nwFrag) {
-        UINT16 dstPort = htons(flowKey->ipKey.l4.tpDst);
+     /*
+      * For some of the tunnel types such as GRE, the dstPort is not applicable
+      * since GRE does not have a L4 port. We use '0' for convenience.
+      */
 
-        ASSERT(flowKey->ipKey.nwProto != IPPROTO_GRE || dstPort == 0);
+    if ((flowKey->l2.dlType == htons(ETH_TYPE_IPV4) &&
+        !flowKey->ipKey.nwFrag) ||
+        (flowKey->l2.dlType == htons(ETH_TYPE_IPV6) &&
+        !flowKey->ipv6Key.nwFrag)) {
+        UINT16 dstPort = 0;
+        uint8_t nwProto = 0;
+        if (flowKey->l2.dlType == htons(ETH_TYPE_IPV6)) {
+            dstPort = htons(flowKey->ipv6Key.l4.tpDst);
+            nwProto = flowKey->ipv6Key.nwProto;
+         } else if (flowKey->l2.dlType == htons(ETH_TYPE_IPV4)) {
+            dstPort = htons(flowKey->ipKey.l4.tpDst);
+            nwProto = flowKey->ipKey.nwProto;
+        }
+        ASSERT(nwProto != IPPROTO_GRE || dstPort == 0);
 
         tunnelVport =
             OvsFindTunnelVportByDstPortAndNWProto(ovsFwdCtx->switchContext,
-                                                  dstPort,
-                                                  flowKey->ipKey.nwProto);
+                                                  dstPort, nwProto);
         if (tunnelVport) {
             switch(tunnelVport->ovsType) {
             case OVS_VPORT_TYPE_STT:
@@ -290,12 +300,12 @@ OvsDetectTunnelPkt(OvsForwardingContext *ovsFwdCtx,
                 (vport->ovsType != OVS_VPORT_TYPE_NETDEV &&
                  vport->ovsType != OVS_VPORT_TYPE_INTERNAL &&
                  !OvsIsTunnelVportType(vport->ovsType))) {
-                ovsFwdCtx->tunKey.dst = 0;
+                RtlZeroMemory(&ovsFwdCtx->tunKey.dst, sizeof(ovsFwdCtx->tunKey.dst));
             }
         }
 
         /* Tunnel the packet only if tunnel context is set. */
-        if (ovsFwdCtx->tunKey.dst != 0) {
+        if (!OvsIphIsZero(&(ovsFwdCtx->tunKey.dst))) {
             switch(dstVport->ovsType) {
             case OVS_VPORT_TYPE_GRE:
                 ovsActionStats.txGre++;
@@ -470,7 +480,7 @@ static __inline VOID
 OvsClearTunTxCtx(OvsForwardingContext *ovsFwdCtx)
 {
     ovsFwdCtx->tunnelTxNic = NULL;
-    ovsFwdCtx->tunKey.dst = 0;
+    RtlZeroMemory(&ovsFwdCtx->tunKey.dst, sizeof(ovsFwdCtx->tunKey.dst));
 }
 
 
@@ -484,7 +494,7 @@ static __inline VOID
 OvsClearTunRxCtx(OvsForwardingContext *ovsFwdCtx)
 {
     ovsFwdCtx->tunnelRxNic = NULL;
-    ovsFwdCtx->tunKey.dst = 0;
+    RtlZeroMemory(&ovsFwdCtx->tunKey.dst, sizeof(ovsFwdCtx->tunKey.dst));
 }
 
 
@@ -540,7 +550,7 @@ OvsCompleteNBLForwardingCtx(OvsForwardingContext *ovsFwdCtx,
  * --------------------------------------------------------------------------
  */
 static __inline NDIS_STATUS
-OvsDoFlowLookupOutput(OvsForwardingContext *ovsFwdCtx)
+OvsDoFlowLookupOutput(OvsForwardingContext* ovsFwdCtx)
 {
     OvsFlowKey key = { 0 };
     OvsFlow *flow = NULL;
@@ -557,11 +567,11 @@ OvsDoFlowLookupOutput(OvsForwardingContext *ovsFwdCtx)
     ASSERT(vport->nicState == NdisSwitchNicStateConnected);
 
     /* Assert that in the Rx direction, key is always setup. */
-    ASSERT(ovsFwdCtx->tunnelRxNic == NULL || ovsFwdCtx->tunKey.dst != 0);
+    ASSERT(ovsFwdCtx->tunnelRxNic == NULL || !OvsIphIsZero(&(ovsFwdCtx->tunKey.dst)));
     status =
         OvsExtractFlow(ovsFwdCtx->curNbl, ovsFwdCtx->srcVportNo,
                        &key, &ovsFwdCtx->layers,
-                       ovsFwdCtx->tunKey.dst != 0 ? &ovsFwdCtx->tunKey : NULL);
+                       !OvsIphIsZero(&(ovsFwdCtx->tunKey.dst))? &(ovsFwdCtx->tunKey):NULL);
     if (status != NDIS_STATUS_SUCCESS) {
         OvsCompleteNBLForwardingCtx(ovsFwdCtx,
                                     L"OVS-Flow extract failed");
@@ -647,6 +657,7 @@ OvsTunnelPortTx(OvsForwardingContext *ovsFwdCtx)
         OvsDoFragmentNbl(ovsFwdCtx, ctx->mru);
     }
     OVS_FWD_INFO switchFwdInfo = { 0 };
+
     /* Apply the encapsulation. The encapsulation will not consume the NBL. */
     switch(ovsFwdCtx->tunnelTxNic->ovsType) {
     case OVS_VPORT_TYPE_GRE:
@@ -915,11 +926,11 @@ OvsOutputForwardingCtx(OvsForwardingContext *ovsFwdCtx)
     if (ovsFwdCtx->tunnelTxNic != NULL) {
         status = OvsTunnelPortTx(ovsFwdCtx);
         ASSERT(ovsFwdCtx->tunnelTxNic == NULL);
-        ASSERT(ovsFwdCtx->tunKey.dst == 0);
+        ASSERT(OvsIphIsZero(&(ovsFwdCtx->tunKey.dst)));
     } else if (ovsFwdCtx->tunnelRxNic != NULL) {
         status = OvsTunnelPortRx(ovsFwdCtx);
         ASSERT(ovsFwdCtx->tunnelRxNic == NULL);
-        ASSERT(ovsFwdCtx->tunKey.dst == 0);
+        ASSERT(OvsIphIsZero(&(ovsFwdCtx->tunKey.dst)));
     }
     ASSERT(ovsFwdCtx->curNbl == NULL);
 
@@ -1753,10 +1764,10 @@ OvsExecuteSetAction(OvsForwardingContext *ovsFwdCtx,
 
     case OVS_KEY_ATTR_TUNNEL:
     {
-        OvsIPv4TunnelKey tunKey;
+        OvsIPTunnelKey tunKey = { 0 };
         tunKey.flow_hash = (uint16)(hash ? *hash : OvsHashFlow(key));
         tunKey.dst_port = key->ipKey.l4.tpDst;
-        NTSTATUS convertStatus = OvsTunnelAttrToIPv4TunnelKey((PNL_ATTR)a, &tunKey);
+        NTSTATUS convertStatus = OvsTunnelAttrToIPTunnelKey((PNL_ATTR)a, &tunKey);
         status = SUCCEEDED(convertStatus) ? NDIS_STATUS_SUCCESS : NDIS_STATUS_FAILURE;
         ASSERT(status == NDIS_STATUS_SUCCESS);
         RtlCopyMemory(&ovsFwdCtx->tunKey, &tunKey, sizeof ovsFwdCtx->tunKey);
@@ -1874,7 +1885,7 @@ OvsOutputUserspaceAction(OvsForwardingContext *ovsFwdCtx,
     POVS_PACKET_HDR_INFO layers = &ovsFwdCtx->layers;
     BOOLEAN isRecv = FALSE;
     OVS_FWD_INFO fwdInfo;
-    OvsIPv4TunnelKey tunKey;
+    OvsIPTunnelKey tunKey;
 
     POVS_VPORT_ENTRY vport = OvsFindVportByPortNo(ovsFwdCtx->switchContext,
                                                   ovsFwdCtx->srcVportNo);
@@ -1894,10 +1905,11 @@ OvsOutputUserspaceAction(OvsForwardingContext *ovsFwdCtx,
     if (egrTunAttr) {
         RtlZeroMemory(&tunKey,  sizeof tunKey);
         RtlCopyMemory(&tunKey, &ovsFwdCtx->tunKey, sizeof tunKey);
-        if (!tunKey.src) {
-            status = OvsLookupIPFwdInfo(tunKey.src, tunKey.dst, &fwdInfo);
-            if (status == NDIS_STATUS_SUCCESS && tunKey.dst == fwdInfo.dstIpAddr) {
-                tunKey.src = fwdInfo.srcIpAddr;
+        if (!OvsIphIsZero(&(tunKey.src))) {
+            status = OvsLookupIPhFwdInfo(tunKey.src, tunKey.dst, &fwdInfo);
+            if (status == NDIS_STATUS_SUCCESS &&
+                OvsIphAddrEquals(&(tunKey.dst), &(fwdInfo.dstIphAddr))) {
+                OvsCopyIphAddress(&(tunKey.src), &fwdInfo.srcIphAddr);
             }
         }
         tunKey.flow_hash = tunKey.flow_hash ? tunKey.flow_hash : MAXINT16;

datapath-windows/ovsext/Actions.h
@@ -58,7 +58,7 @@ typedef struct OvsForwardingContext {
      * - specified in actions during tunneling Tx
      * - extracted from an NBL during tunneling Rx
      */
-    OvsIPv4TunnelKey tunKey;
+    OvsIPTunnelKey tunKey;
 
     /*
      * Tunneling - Tx:

datapath-windows/ovsext/Conntrack.c
@@ -20,6 +20,7 @@
 #include "PacketParser.h"
 #include "Event.h"
 #include "Conntrack-nat.h"
+#include "IpHelper.h"
 
 #pragma warning(push)
 #pragma warning(disable:4311)
@@ -511,7 +512,7 @@ OvsDetectCtPacket(OvsForwardingContext *fwdCtx,
                  status =
                       OvsExtractFlow(fwdCtx->curNbl, fwdCtx->srcVportNo,
                                      &newFlowKey, &fwdCtx->layers,
-                                     fwdCtx->tunKey.dst != 0 ? &fwdCtx->tunKey : NULL);
+                                     !OvsIphIsZero(&(fwdCtx->tunKey.dst)) ? &(fwdCtx->tunKey) : NULL);
                 if (status != NDIS_STATUS_SUCCESS) {
                      OVS_LOG_ERROR("Extract flow failed Nbl %p", fwdCtx->curNbl);
                      return status;

datapath-windows/ovsext/DpInternal.h
@@ -137,7 +137,7 @@ typedef struct L2Key {
 #define NUM_PKT_ATTR_REQUIRED 35
 #define TUN_OPT_MAX_LEN 255
 
-typedef union OvsIPv4TunnelKey {
+typedef union OvsIPTunnelKey {
     /* Options should always be the first member of tunnel key.
      * They are stored at the end of the array if they are less than the
      * maximum size. This allows us to get the benefits of variable length
@@ -146,8 +146,9 @@ typedef union OvsIPv4TunnelKey {
     struct {
         UINT8 tunOpts[TUN_OPT_MAX_LEN];          /* Tunnel options. */
         UINT8 tunOptLen;             /* Tunnel option length in byte. */
-        ovs_be32 dst;
-        ovs_be32 src;
+
+        SOCKADDR_INET dst;     /* IPv4/6 destination address. */
+        SOCKADDR_INET src;     /* IPv4/6 source address. */
         ovs_be64 tunnelId;
         uint16_t flags;
         uint8_t  tos;
@@ -161,24 +162,24 @@ typedef union OvsIPv4TunnelKey {
         };
     };
     uint64_t attr[NUM_PKT_ATTR_REQUIRED];
-} OvsIPv4TunnelKey; /* Size of 280 byte. */
+} OvsIPTunnelKey; /* Size of 280+40-8= 312 byte. */
 
 static __inline uint8_t
-TunnelKeyGetOptionsOffset(const OvsIPv4TunnelKey *key)
+IPTunnelKeyGetOptionsOffset(const OvsIPTunnelKey *key)
 {
     return TUN_OPT_MAX_LEN - key->tunOptLen;
 }
 
 static __inline uint8_t *
-TunnelKeyGetOptions(OvsIPv4TunnelKey *key)
+IPTunnelKeyGetOptions(OvsIPTunnelKey *key)
 {
-    return key->tunOpts + TunnelKeyGetOptionsOffset(key);
+    return key->tunOpts + IPTunnelKeyGetOptionsOffset(key);
 }
 
 static __inline uint16_t
-TunnelKeyGetRealSize(OvsIPv4TunnelKey *key)
+IPTunnelKeyGetRealSize(OvsIPTunnelKey *key)
 {
-    return sizeof(OvsIPv4TunnelKey) - TunnelKeyGetOptionsOffset(key);
+    return sizeof(OvsIPTunnelKey) - IPTunnelKeyGetOptionsOffset(key);
 }
 
 typedef struct MplsKey {
@@ -187,7 +188,7 @@ typedef struct MplsKey {
 } MplsKey; /* Size of 8 bytes. */
 
 typedef __declspec(align(8)) struct OvsFlowKey {
-    OvsIPv4TunnelKey tunKey;     /* 280 bytes */
+    OvsIPTunnelKey tunKey;     /* 280 bytes? */
     L2Key l2;                    /* 32 bytes */
     union {
         /* These headers are mutually exclusive. */
@@ -209,7 +210,7 @@ typedef __declspec(align(8)) struct OvsFlowKey {
     } ct;                        /* Connection Tracking Flags */
 } OvsFlowKey;
 
-#define OVS_WIN_TUNNEL_KEY_SIZE (sizeof (OvsIPv4TunnelKey))
+#define OVS_WIN_IP_TUNNEL_KEY_SIZE (sizeof (OvsIPTunnelKey))
 #define OVS_L2_KEY_SIZE (sizeof (L2Key))
 #define OVS_IP_KEY_SIZE (sizeof (IpKey))
 #define OVS_IPV6_KEY_SIZE (sizeof (Ipv6Key))
@@ -295,7 +296,7 @@ typedef struct _OVS_PACKET_INFO {
     uint32_t queue;
     uint32_t inPort;
     uint32_t cmd;
-    OvsIPv4TunnelKey tunnelKey;
+    OvsIPTunnelKey tunnelKey;
     uint8_t *payload;
     /* Includes user data defined as chain of netlink attributes followed by the
      * packet data. */

commit 6245d295330476331bc5d99a5d1f792105f6d70d

datapath-windows: Fix GRE tunnel.
GRE tunnel RX is broken due to incorrecly checking the
'tunKey->dst.si_family != AF_INET', which is actually
set later after parsing the GRE header. Removing such
chunk makes GRE tunnel works.

Fixes: edb2335861d6 ("datapath-windows: Add IPv6 Geneve tunnel support in Windows")
Cc: Alin-Gabriel Serdean <aserdean@ovn.org>
Signed-off-by: William Tu <u9012063@gmail.com>

datapath-windows/ovsext/Gre.c
@@ -332,11 +332,6 @@ OvsDecapGre(POVS_SWITCH_CONTEXT switchContext,
 
     *newNbl = NULL;
 
-    if (tunKey->dst.si_family != AF_INET) {
-        /*V6 tunnel support will be supported later*/
-        return NDIS_STATUS_FAILURE;
-    }
-
     status = OvsExtractLayers(curNbl, &layers);
     if (status != NDIS_STATUS_SUCCESS) {
         return status;